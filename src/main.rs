709cf14ec43492ce4e3eae93af7d78e28789a46a
Zach Wong <zach.wong@uniswap.org>
2025-04-28 22:49:27 -0700

    added transaction receipt retrieval

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use alloy_rpc_client::ClientBuilder;
+use clap::Parser;
+use dotenv::dotenv;
+use std::env;
+use url::Url;
+use alloy_rpc_types::TransactionReceipt;
+
+#[derive(Parser)]
+struct Args {
+    #[clap(short, long)]
+    transaction_hash: String,
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let rpc_url =
+        Url::parse(&env::var("UNICHAIN_RPC").expect("need to set Unichain RPC in dotenv"))
+            .expect("invalid RPC URL");
+    
+    println!("Parsed URL: {}", rpc_url);
+
+    let client = ClientBuilder::default().http(rpc_url);
+
+    let args = Args::parse();
+    let untrusted_transaction_hash = args.transaction_hash;
+
+    let request = client.request("eth_getTransactionReceipt", (untrusted_transaction_hash,));
+
+    let receipt: TransactionReceipt = request.await.unwrap();
+    println!("receipt: {:?}", receipt);
 }
